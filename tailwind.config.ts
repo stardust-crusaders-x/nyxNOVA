f200a4fb5d9e7ffde787231a8238d05273574730
bluestorm <srijadutta0420@gmail.com>
2024-07-18 18:08:30 +0530

    changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+       colors: {
+        'blackish-green': '#001a16', // Example custom blackish green color
+        },
+        fontFamily:{
+          body: ["var(--font-novo-mono)"],
+          display: ["var(--font-roboto-mono)"]
+        }
     },
   },
   plugins: [],
